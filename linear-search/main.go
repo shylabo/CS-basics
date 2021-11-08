6e624713db005eea117d47bd02cd71934f799a7e
shylabo <shuya.sohot@gmail.com>
2021-11-09 00:08:01 +0900

    linear-search

---
+++ linear-search/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func linearSearch(a []int, key int) bool {
+	for _, item := range a {
+		if item == key {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	result := linearSearch([]int{1, 2, 3, 4, 5}, 4)
+
+	if result {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}
